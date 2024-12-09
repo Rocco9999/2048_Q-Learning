fe1486be2248afd9b84f9f76ac7b50f3a81342c9
Rocco9999 <37149871+Rocco9999@users.noreply.github.com>
2024-12-09 14:15:05 +0100

    new file:   environment/Game2048_env.py 	new file:   environment/__init__.py 	new file:   main.py 	new file:   requirements.txt

---
+++ environment/Game2048_env.py
@@ -0,0 +1,6 @@
+import gymnasium as gym
+import numpy as np
+
+class Game2048Env(gym.Env):
+    # Definizione dell'ambiente
+    print("Ciao")
---
+++ main.py
@@ -0,0 +1,6 @@
+from environment.Game2048_env import Game2048Env
+
+if __name__ == "__main__":
+    env = Game2048Env()
+    env.reset()
+    # Interazione o addestramento
